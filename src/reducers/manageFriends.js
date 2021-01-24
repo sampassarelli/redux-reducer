8d3146a5a372e1addd9f5e6ee548e8895efbd15e
sampassarelli <passarelli.sam@gmail.com>
2021-01-24 17:22:39 -0600

    lab sumission

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch(action.type){
+      case "ADD_FRIEND":
+          return {friends: [...state.friends, action.friend]}
+      case "REMOVE_FRIEND":
+          const newFriends = [...state.friends].filter(friend => friend.id !== action.id)
+          state = { friends: newFriends}
+  }
+  return state
 }
